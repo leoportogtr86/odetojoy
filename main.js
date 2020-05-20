a1fa89597a1b90feccdfd87944c8ddb033af843d
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2020-05-20 13:16:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,286 @@
+var synth = new Tone.Synth().toDestination()
+let pad = document.querySelector('#pad')
+let nota1 = document.querySelector('#nota1')
+let nota2 = document.querySelector('#nota2')
+let nota3 = document.querySelector('#nota3')
+let nota4 = document.querySelector('#nota4')
+let nota5 = document.querySelector('#nota5')
+let nota6 = document.querySelector('#nota6')
+let nota7 = document.querySelector('#nota7')
+let nota8 = document.querySelector('#nota8')
+let nota9 = document.querySelector('#nota9')
+let nota10 = document.querySelector('#nota10')
+let nota11 = document.querySelector('#nota11')
+let nota12 = document.querySelector('#nota12')
+let nota13 = document.querySelector('#nota13')
+let nota14 = document.querySelector('#nota14')
+let nota15 = document.querySelector('#nota15')
+let nota16 = document.querySelector('#nota16')
+let nota17 = document.querySelector('#nota17')
+let nota18 = document.querySelector('#nota18')
+let nota19 = document.querySelector('#nota19')
+let nota20 = document.querySelector('#nota20')
+let nota21 = document.querySelector('#nota21')
+let nota22 = document.querySelector('#nota22')
+let nota23 = document.querySelector('#nota23')
+let nota24 = document.querySelector('#nota24')
+let nota25 = document.querySelector('#nota25')
+let nota26 = document.querySelector('#nota26')
+let nota27 = document.querySelector('#nota27')
+let nota28 = document.querySelector('#nota28')
+let nota29 = document.querySelector('#nota29')
+let nota30 = document.querySelector('#nota30')
+
+nota1.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota1.style.display = 'none'
+    nota2.style.display = 'block'
+    pad.play()
+}
+
+
+nota2.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota2.style.display = 'none'
+    nota3.style.display = 'block'
+}
+
+
+nota3.onclick = function () {
+
+    synth.triggerAttackRelease('F4', '8n')
+    nota3.style.display = 'none'
+    nota4.style.display = 'block'
+}
+
+
+nota4.onclick = function () {
+
+    synth.triggerAttackRelease('G4', '8n')
+    nota4.style.display = 'none'
+    nota5.style.display = 'block'
+}
+
+nota5.onclick = function () {
+
+    synth.triggerAttackRelease('G4', '8n')
+    nota5.style.display = 'none'
+    nota6.style.display = 'block'
+}
+
+
+nota6.onclick = function () {
+
+    synth.triggerAttackRelease('F4', '8n')
+    nota6.style.display = 'none'
+    nota7.style.display = 'block'
+}
+
+
+nota7.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota7.style.display = 'none'
+    nota8.style.display = 'block'
+}
+
+
+nota8.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota8.style.display = 'none'
+    nota9.style.display = 'block'
+}
+
+
+nota9.onclick = function () {
+
+    synth.triggerAttackRelease('C4', '8n')
+    nota9.style.display = 'none'
+    nota10.style.display = 'block'
+}
+
+
+nota10.onclick = function () {
+
+    synth.triggerAttackRelease('C4', '8n')
+    nota10.style.display = 'none'
+    nota11.style.display = 'block'
+}
+
+
+nota11.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota11.style.display = 'none'
+    nota12.style.display = 'block'
+}
+
+
+nota12.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota12.style.display = 'none'
+    nota13.style.display = 'block'
+}
+
+
+nota13.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota13.style.display = 'none'
+    nota14.style.display = 'block'
+}
+
+
+nota14.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota14.style.display = 'none'
+    nota15.style.display = 'block'
+}
+
+
+nota15.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota15.style.display = 'none'
+    nota16.style.display = 'block'
+}
+
+
+
+nota16.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota16.style.display = 'none'
+    nota17.style.display = 'block'
+}
+
+
+
+nota17.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota17.style.display = 'none'
+    nota18.style.display = 'block'
+}
+
+
+
+nota18.onclick = function () {
+
+    synth.triggerAttackRelease('F4', '8n')
+    nota18.style.display = 'none'
+    nota19.style.display = 'block'
+}
+
+
+
+nota19.onclick = function () {
+
+    synth.triggerAttackRelease('G4', '8n')
+    nota19.style.display = 'none'
+    nota20.style.display = 'block'
+}
+
+
+
+nota20.onclick = function () {
+
+    synth.triggerAttackRelease('G4', '8n')
+    nota20.style.display = 'none'
+    nota21.style.display = 'block'
+}
+
+
+
+nota21.onclick = function () {
+
+    synth.triggerAttackRelease('F4', '8n')
+    nota21.style.display = 'none'
+    nota22.style.display = 'block'
+}
+
+
+
+nota22.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota22.style.display = 'none'
+    nota23.style.display = 'block'
+}
+
+
+
+nota23.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota23.style.display = 'none'
+    nota24.style.display = 'block'
+}
+
+
+
+nota24.onclick = function () {
+
+    synth.triggerAttackRelease('C4', '8n')
+    nota24.style.display = 'none'
+    nota25.style.display = 'block'
+}
+
+
+
+nota25.onclick = function () {
+
+    synth.triggerAttackRelease('C4', '8n')
+    nota25.style.display = 'none'
+    nota26.style.display = 'block'
+}
+
+
+
+nota26.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota26.style.display = 'none'
+    nota27.style.display = 'block'
+}
+
+
+
+nota27.onclick = function () {
+
+    synth.triggerAttackRelease('E4', '8n')
+    nota27.style.display = 'none'
+    nota28.style.display = 'block'
+}
+
+
+
+nota28.onclick = function () {
+
+    synth.triggerAttackRelease('D4', '8n')
+    nota28.style.display = 'none'
+    nota29.style.display = 'block'
+}
+
+
+
+nota29.onclick = function () {
+
+    synth.triggerAttackRelease('C4', '8n')
+    nota29.style.display = 'none'
+    nota30.style.display = 'block'
+}
+
+
+
+nota30.onclick = function () {
+
+    synth.triggerAttackRelease('C4', '8n')
+    nota30.style.display = 'none'
+    //nota31.style.display = 'block'
+}
